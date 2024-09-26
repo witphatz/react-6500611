3538a96dc530e26914238081756e4d4eca3cabca
witphatz <azen555666@gmail.com>
2024-09-26 10:41:06 +0700

    add my name

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
   return (
     <div>
       <MyBar name="Me App" />
-      <h1>Hello DIT, RSU</h1>
+      <h1>Witphat Hanwet 6500611</h1>
       <Grid container spacing={2}>
       {data.map(item => (
         <Grid item xs={12} md={4}>
